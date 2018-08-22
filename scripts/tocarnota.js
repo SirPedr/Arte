6d5fc3d3c88ffd35be5c90ccf5e5e2660095b44e
Pedro Paulo <ppagbpedro@gmail.com>
2018-08-22 13:04:06 -0300

    Melhoras no script
    Em vez de várias e várias ID's, agora as notas são selecionadas dinamicamente e não podem ser tocadas ao mesmo tempo.

--- scripts/tocarnota.js
+++ scripts/tocarnota.js
@@ -1,72 +1,21 @@
-let TocarNota = document.querySelector("#botao-nota"),
-    La = document.getElementById("NotaA"),
-    Si = document.getElementById("NotaB"),
-    Do = document.getElementById("NotaC"),
-    Re = document.getElementById("NotaD"),
-    Mi = document.getElementById("NotaE"),
-    Fa = document.getElementById("NotaF"),
-   Sol = document.getElementById("NotaG"),
-   i = 0;
 
+let botaoTocarNota = document.querySelector("#botao-nota"),
+    selectOpcoesNota = document.querySelector('select'),
+    audioNotaAnterior = new Audio("audio/NotaA.mp3"),
+    auxContador = 1;
 
-TocarNota.addEventListener('click', function(){
-  let InputNota = document.querySelector("#input-nota").value;
-      i++;
-  if(InputNota =='Dó'){
-    TocarNota.innerHTML = "Pausar";
-    if(i % 2 == 1){
-      Do.play();
-    }else{
-      Do.pause();
-      TocarNota.innerHTML = 'Tocar';
-    }
-  }else if(InputNota == 'Ré'){
-    TocarNota.innerHTML = "Pausar";
-    if(i % 2 == 1){
-      Re.play();
-    }else{
-      Re.pause();
-      TocarNota.innerHTML = 'Tocar';
-    }
-  }else if(InputNota == 'Mi'){
-    TocarNota.innerHTML = "Pausar";
-    if(i % 2 == 1){
-      Mi.play();
-    }else{
-      Mi.pause();
-      TocarNota.innerHTML = 'Tocar';
-    }
-  }else if(InputNota == 'Fá'){
-    TocarNota.innerHTML = "Pausar";
-    if(i % 2 == 1){
-      Fa .play();
-    }else{
-      Fa.pause();
-      TocarNota.innerHTML = 'Tocar';
-    }
-  }else if(InputNota == 'Sol'){
-    TocarNota.innerHTML = "Pausar";
-    if(i % 2 == 1){
-      Sol.play();
-    }else{
-      Sol.pause();
-      TocarNota.innerHTML = 'Tocar';
-    }
-  }else if(InputNota == 'Lá'){
-    TocarNota.innerHTML = "Pausar";
-    if(i % 2 == 1){
-      La.play();
-    }else{
-      La.pause();
-      TocarNota.innerHTML = 'Tocar';
-    }
-  }else if(InputNota == 'Si'){
-    TocarNota.innerHTML = "Pausar";
-    if(i % 2 == 1){
-      Si.play();
-    }else{
-      Si.pause();
-      TocarNota.innerHTML = 'Tocar';
-    }
-  }
+botaoTocarNota.addEventListener('click', function toggleNotaCorrespondente(){
+   audioNotaAnterior.pause();
+
+   let urlAudio = "audio/Nota" + selectOpcoesNota.value + ".mp3",
+       notaEscolhida = new Audio(urlAudio);
+
+   audioNotaAnterior = notaEscolhida;
+
+   if(auxContador % 2 === 0)
+      notaEscolhida.pause();
+   else
+      notaEscolhida.play();
+
+   auxContador++;
 });
